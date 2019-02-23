244abe04a95dc1f11350ec00469771e1199b995d
Sean <seanzee25@gmail.com>
2019-02-23 16:03:45 -0600

    Update Application.java
    Built tree and println for testing output.

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -7,6 +7,10 @@ import MorseTree.MorseCodeTree;
 
 public class Application {
 	public static void main(String[] args) throws IOException {
+	    MorseCodeTree morseCodeTree = MorseCodeTree.buildFromFile("input.txt");
+	    System.out.println(morseCodeTree); // TODO: See toString method to explain output. Only printing here for testing.
+	    
 		Menu.promptAction();
+	    
 	}
 }
