1bc874bdbcec3a426ab935a4c94db537d43e0051
Sean <seanzee25@gmail.com>
2019-02-23 16:03:21 -0600

    Update MorseCodeTree.java
    Implemented buildFromFile method, and toString method. See toString comments for output format.

--- src/MorseTree/MorseCodeTree.java
+++ src/MorseTree/MorseCodeTree.java
@@ -1,6 +1,8 @@
 package MorseTree;
 
-import Files.FileHandler;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MorseCodeTree {
 	protected class BTNode {
@@ -32,23 +34,80 @@ public class MorseCodeTree {
 		root.right = right.root;
 	}
 	
+	/**
+	 * 
+	 * @param fileName Name of the input file
+	 * @return The MorseCodeTree filled with the data from file
+	 * @throws IOException
+	 */
+	public static MorseCodeTree buildFromFile(String fileName) throws IOException {
+	    FileInputStream fileInput = new FileInputStream(fileName);
+	    Scanner input = new Scanner(fileInput);
+	    
+	    // Initialize tree with root node null
+	    MorseCodeTree theTree = new MorseCodeTree();
+	    theTree.root = theTree.new BTNode();
+	    input.forEachRemaining(line -> { // For each line in file
+	        BTNode curNode = theTree.root;
+	        char current = line.charAt(0);
+	        String morseValue = line.substring(1);
+	        char morseToken;
+	        
+	        for(int i = 0; i < morseValue.length(); i++) { // Iterate through morse tokens
+	            morseToken = morseValue.charAt(i);
+	            if(morseToken == '.') { // Go left
+	                if(curNode.left == null) { curNode.left = theTree.new BTNode(); } // If it's null, make null node
+	                curNode = curNode.left;
+	            }
+	            if(morseToken == '-') { // Go right
+	                if(curNode.right == null) { curNode.right = theTree.new BTNode(); } // If it's null, make null node
+	                curNode = curNode.right;
+	            }
+	        }
+	        curNode.data = current; // After looping morse tokens, we are at our destination.
+	    });
+	    
+	    fileInput.close();
+	    input.close();
+	    
+	    return theTree;
+	}
 	
-	
-	public String decodeMessage(String message) {
+	public static String decodeMessage(String message) {
 		// TODO: Decode message and return it using constructed binary tree
 		return message;
 	}
 	
-	public String encodeMessage(String message) {
+	public static String encodeMessage(String message) {
 		// TODO: Encode message in morse code using binary tree and return it.
 		return message;
 	}
 	
+	/**
+	 * Takes a StringBuilder object and formulates a string representation
+	 * of the tree using preorder traversal
+	 * @param stringBuilder StringBuilder to append the data to.
+	 * @param root The root of the tree
+	 */
+	private void preorderString(StringBuilder stringBuilder, BTNode root) {
+	    stringBuilder.append(root.data);
+	    if(root.left != null) {
+	        preorderString(stringBuilder, root.left);
+	    }
+	    if(root.right != null) {
+	        preorderString(stringBuilder, root.right);
+	    }
+	}
+	
 	@Override 
+	/**
+	 * Outputs the tree using preorder traversal
+	 * In our case, the string should return as " eishvufarlwpjtndbxkcymgzqo"
+	 */
 	public String toString() {
-		// We should just print some value using one of the traversal methods as a helper function
-	    // to build a string
-	    return null;
+		StringBuilder stringBuilder = new StringBuilder();
+		preorderString(stringBuilder, root);
+	    return stringBuilder.toString();
 	}
 	
 }
