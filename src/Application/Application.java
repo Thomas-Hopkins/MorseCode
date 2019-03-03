9497904a183dcd947b893978bf693534524f8f0a
Sean <seanzee25@gmail.com>
2019-03-02 20:25:24 -0600

    Update Application.java
    Not sure that there's a change here. But commiting just in case.

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -5,21 +5,21 @@ import java.io.IOException;
 import MorseTree.MorseCodeTree;
 
 public class Application {
-	public static void main(String[] args) throws IOException {
-		// Build morse code binary tree and create a menu
-	    MorseCodeTree morseCodeTree = MorseCodeTree.buildFromFile("input.txt");
-	    Menu appMenu = new Menu(morseCodeTree);
-	    
-	    // Main loop
-	    int status;
-	    while(appMenu.running) {
-	    	status = appMenu.promptAction();
-	    	if(status < 0) {
-	    		System.out.println("INVALID INPUT.\n");
-	    	}
-	    	else if (status > 0) {
-	    		System.out.println("Goodbye.");
-	    	}
-	    }
-	}
+    public static void main(String[] args) throws IOException {
+        // Build morse code binary tree and create a menu
+        MorseCodeTree morseCodeTree = MorseCodeTree.buildFromFile("input.txt");
+        Menu appMenu = new Menu(morseCodeTree);
+        
+        // Main loop
+        int status;
+        while(appMenu.running) {
+            status = appMenu.promptAction();
+            if(status < 0) {
+                System.out.println("INVALID INPUT.\n");
+            }
+            else if (status > 0) {
+                System.out.println("Goodbye.");
+            }
+        }
+    }
 }
